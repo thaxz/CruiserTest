3c0217f29a09fc39480c4e5fc3d15286af224253
thaxz <thais.monteiroc@ufpe.br>
2023-02-15 12:32:28 -0300

    first commit

---
+++ CruiserTeste/CruiserTesteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CruiserTesteApp.swift
+//  CruiserTeste
+//
+//  Created by thaxz on 15/02/23.
+//
+
+import SwiftUI
+
+@main
+struct CruiserTesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
